3bbf666022816a8f224f5aa01b70e4207277f6e5
EC2 Default User <ec2-user@ip-172-31-9-83.us-east-2.compute.internal>
2023-02-08 23:25:33 +0000

    Final

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -13,30 +13,51 @@ end
 
 
 def max_2_sum(arr)
-  # YOUR CODE HERE
+  sum(arr.sort.last(2))
 end
 
 def sum_to_n?(arr, number)
-  # YOUR CODE HERE
+  arr.product(arr).any? {|couple| sum(couple) == n}
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
 def starts_with_consonant?(string)
-  # YOUR CODE HERE
+  s =~ /^[bcdfghjklmnpqrstvwxyz]/i
 end
 
 def binary_multiple_of_4?(string)
-  # YOUR CODE HERE
+  if bin = /^[1|0]+/.match(string)
+    bin.to_string.to_i(2) % 4 == 0
+  else
+    false
+  end
 end
+
 
 # Part 3
 
 # Object representing a book
 class BookInStock
-  # YOUR CODE HERE
+  
+  attr_accessor :isbn, :price
+  
+def initialize(isbn, price) 
+
+  if isbn.empty?
+    raise(ArgumentError, "ISBN number is the empty string.")
+  elsif price <= 0
+    raise(ArgumentError, "Price must be positive.")
+  end
+  @isbn = isbn
+  @price = Float(price)
+end
+
+def price_as_string
+  return sprintf "$"+"%.2f", price
+end
 end
